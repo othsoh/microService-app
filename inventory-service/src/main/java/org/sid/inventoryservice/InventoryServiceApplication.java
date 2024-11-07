f33b537903e8d7cae9fbb58413fb42a0d971b836
othsoh <106303256+othsoh@users.noreply.github.com>
2024-11-07 11:17:12 +0100

    Adding the discovery service

--- inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,14 @@
 package org.sid.inventoryservice;
 
+import org.sid.inventoryservice.Repositories.ProductRepository;
+import org.sid.inventoryservice.entities.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import java.util.List;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +17,15 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration) {
+        return args -> {
+            restConfiguration.exposeIdsFor(Product.class);
+            productRepository.saveAll(List.of(
+                    Product.builder().name("Computer").price(1000).quantity(10).build(),
+                    Product.builder().name("Printer").price(200).quantity(5).build(),
+                    Product.builder().name("Smartphone").price(500).quantity(20).build()
+            ));
+        };
+    }
 }
