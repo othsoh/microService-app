0cf817acaa7bf95e073807f08683e77c1d0395ff
othsoh <106303256+othsoh@users.noreply.github.com>
2024-12-08 12:28:08 +0100

    Config service - DONE

--- gateway/src/main/java/org/sid/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/sid/gateway/GatewayApplication.java
@@ -2,6 +2,11 @@ package org.sid.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -11,4 +16,20 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+//    @Bean
+/*
+    public RouteLocator staticRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route(r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+                .build();
+    }
+
+*/
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator dynamicRoute(ReactiveDiscoveryClient discoveryClient,
+                                                              DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+    }
 }
